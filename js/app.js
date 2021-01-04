a84cbabd716539e180e443421fce2bd8f26f25bb
talaemad <talae.ishream@gmail.com>
2021-01-04 15:26:18 +0200

    edited lab02

--- js/app.js
+++ js/app.js
@@ -5,51 +5,51 @@ alert('Hello '+ uName+'! Welcome to My Webpage, I hope you enjoy with it :) Now 
 
 var fcolor = validation('Do you think my favourite color is Blue !? Answer with y(yes) or n(no)');
 if(fcolor === 'y'||fcolor === 'yes'){
-  console.log('Correct! Blue always will be my favorite');
+ // console.log('Correct! Blue always will be my favorite');
   alert('Correct! Blue always will be my favorite');
 }
 else if (fcolor === 'n'||fcolor === 'no'){
-  console.log('Why! Blue always will be my favorite');
+  //console.log('Why! Blue always will be my favorite');
   alert('Why! Blue always will be my favorite');
 }
 
 var luckNum = validation('Do you think my luck number is 10 !? Answer with y(yes) or n(no) ');
 if(luckNum === 'y'||luckNum === 'yes'){
-  console.log('No no no ! Its 3');
+  //console.log('No no no ! Its 3');
   alert('No no no ! Its 3');
 }
 else if (luckNum === 'n'||luckNum === 'no'){
-  console.log('Correct! Its 3');
+  //console.log('Correct! Its 3');
   alert('Correct! Its 3');
 }
 
 var fCountry = validation('Do you think USA my favourite country !? Answer with y(yes) or n(no) ');
 if(fCountry === 'y'||fCountry === 'yes'){
-  console.log('No! Its Japan!');
+  //console.log('No! Its Japan!');
   alert('No! Its Japan!');
 }
 else if (fCountry === 'n'||fCountry === 'no'){
-  console.log('Perfect! Its Japan');
+ //console.log('Perfect! Its Japan');
   alert('Perfect! Its Japan');
 }
 
 var anime = validation('Do you think I watch anime !? Answer with y(yes) or n(no) ');
 if(anime === 'y'||anime === 'yes'){
-  console.log('Good Job!!');
+  //console.log('Good Job!!');
   alert('Good Job!!');
 }
 else if (anime === 'n'||anime === 'no'){
-  console.log('Oh no! of course, I do');
+  //console.log('Oh no! of course, I do');
   alert('Oh no! of course, I do ');
 }
 
 var fBird = validation('Do you think Owl my favorite bird?!? Answer with y(yes) or n(no) ');
 if(fBird === 'y'||fBird === 'yes'){
-  console.log('Good Job!!');
+  //console.log('Good Job!!');
   alert('Good Job!!');
 }
 else if (fBird === 'n'||fBird === 'no'){
-  console.log('Soryy! It is');
+  //console.log('Soryy! It is');
   alert('Soryy! It is');
 }
 
